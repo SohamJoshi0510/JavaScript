d753153c227065ff4a1a7739d727ff10751a6e31
SohamJoshi0510 <132748258+SohamJoshi0510@users.noreply.github.com>
2024-09-21 06:16:55 +0000

    data types in JavaScript

--- 01_BASICS/01_variables.js
+++ 01_BASICS/01_variables.js
@@ -2,3 +2,5 @@ const accountId = 14453
 let accountEmail = "hitesh@google.com"
 var accountPassword = "12345"
 accountCity = "Jaipur"
+let accountAddress;
+console.log(accountAddress) // undefined
---
+++ 01_BASICS/02_dataTypes.js
@@ -0,0 +1,12 @@
+"use strict"; // treat all JS code as newer version
+let rno = 92
+let name = "Soham Joshi"
+let isboy = true 
+// number => range: 2 ** 53
+// then use bigint
+
+// symbol: react
+// object
+// null is of object type
+// undefined is of undefined type
+
